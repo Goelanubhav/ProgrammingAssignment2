5bfb192ed19b3500b17af370b3061b58369ae227
Anubhav Goel <goel.anubhav@gmail.com>
2017-08-17 15:50:28 -0700

    Modified to remove git commands

--- cachematrix.R
+++ cachematrix.R
@@ -3,28 +3,35 @@
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
+  ## function to set the matrix values
   set <- function(y) {
     print(class(y))
     x <<- y
     inv <<- NULL
   }
+  ## function to get the matrix values
   get <- function() x
+  ## Function to set the inverse matrix
   setinv <- function(inverse) inv <<- inverse
+  ## Function to get the inverse matrix
   getinv <- function() inv
   list(set = set, get = get, setinv = setinv,getinv = getinv)
 }
 
 
-## Function to validate if data can be derived from cache or to be calculated
+## Function to validate if inverse matrix data is derived from cache or calculated
 
 cacheSolve <- function(x, ...) {
+  
   ## Return a matrix that is the inverse of 'x'
+  ## try to get the cached data
   inv <- x$getinv()
   if(!is.null(inv)) {
     message("getting cached data")
     return(inv)
   }
 
+## If cache data not found calculate the inverse of matrix and return
   data <- x$get()
   inv <- solve(data)
   x$setinv(inv)
@@ -40,7 +47,3 @@ cacheSolve <- function(x, ...) {
 #[,1]       [,2]
 #[1,] -0.3333333  0.6666667
 #[2,]  0.6666667 -0.3333333
-
-
-##git clone git@github.com:goelanubhav7/ProgrammingAssignment2.git /c/SCm/coursera/DST-1/ProgrammingAssignment3-master
-##git remote add upstream https://github.com/goelanubhav7/ProgrammingAssignment2.git
